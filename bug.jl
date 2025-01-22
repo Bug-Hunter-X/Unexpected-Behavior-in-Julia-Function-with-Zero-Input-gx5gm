324bc87f08d6e229de84c2d8bcbf41fa12fc7b23
BugHunter-X <askzenai@gmail.com>
2025-01-22 13:30:04 +0100

    Add bug.jl file

---
+++ bug.jl
@@ -0,0 +1,15 @@
+```julia
+function myfunction(x)
+  if x > 0
+    return x^2
+  else
+    return -x
+  end
+end
+
+println(myfunction(2))  # Output: 4
+println(myfunction(-3)) # Output: 3
+
+# Unexpected behavior
+println(myfunction(0))   # Output: 0
+```
